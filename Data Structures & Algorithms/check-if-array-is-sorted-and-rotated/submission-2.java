43506ad225a1b55e40124c7f78dee618ea8a6ccd
Sairaj Nagula <nagulasairaj16@gmail.com>
2026-07-27 15:10:03 +0530

    Add: check-if-array-is-sorted-and-rotated - submission-2

---
+++ Data Structures & Algorithms/check-if-array-is-sorted-and-rotated/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int count=0;
+        int n = nums.length;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1]){
+                count++;
+            }
+        }
+        if(nums[0]<nums[n-1]){
+            count++;
+        }
+        return count==1;
+    }
+}

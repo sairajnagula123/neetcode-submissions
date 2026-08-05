b2ddbb6fd8e6bcd9e83e85c1a3449aac363ee6f6
Sairaj Nagula <nagulasairaj16@gmail.com>
2026-08-05 15:29:35 +0530

    Add: find-pivot-index - submission-7

---
+++ Data Structures & Algorithms/find-pivot-index/submission-7.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int leftSum=0;
+        int totalSum=0;
+        int rightSum = 0;
+        for(int i=0;i<nums.length;i++){
+            totalSum = totalSum + nums[i];
+        }
+
+        for(int i=0;i<nums.length;i++){
+            rightSum = totalSum - leftSum - nums[i];
+
+            if(rightSum == leftSum){
+                return i;
+            }
+            leftSum = leftSum + nums[i];
+        }
+        return -1;
+        
+    }
+}
